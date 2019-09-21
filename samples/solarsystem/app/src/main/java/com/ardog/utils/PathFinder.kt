ba488d51dcc5535908b50a4a563fb6cf8c44df7f
uchia <uchiachenpro@gmail.com>
2019-09-21 10:22:40 +0800

    增加最短路径接口

---
+++ samples/solarsystem/app/src/main/java/com/ardog/utils/PathFinder.kt
@@ -0,0 +1,18 @@
+package com.ardog.utils
+
+import com.ardog.model.DogPoint
+
+class PathFinder {
+
+    fun initMap(points : List<DogPoint>){
+
+    }
+
+    fun findRoutes(start : DogPoint, destination : DogPoint) : List<DogPoint>{
+        return ArrayList()
+    }
+
+    fun findRoutes(startPointName : String ,destinationName: String) : List<DogPoint>{
+        return ArrayList()
+    }
+}
