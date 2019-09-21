55e64507aeb494f1014eb4ddb877d013a5c3d735
nb312 <nb312@163.com>
2019-09-21 11:44:15 +0800

    fix:添加交互

--- samples/solarsystem/app/src/main/java/com/ardog/model/DogPoint.java
+++ samples/solarsystem/app/src/main/java/com/ardog/model/DogPoint.java
@@ -1,6 +1,8 @@
 package com.ardog.model;
 
 
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.gson.Gson;
 
 /**
@@ -33,6 +35,18 @@ public class DogPoint {
     static public DogPoint parse(String json) {
         Gson gson = new Gson();
         return gson.fromJson(json, DogPoint.class);
+    }
+    public Vector3 toPositionVector(){
+        if(position==null || position.length!=3)return Vector3.zero();
+        return new Vector3(position[0],position[1],position[2]);
+    }
+    public Vector3 toRotationVector(){
+        if(rotation==null || rotation.length!=4)return Vector3.zero();
+        return new Vector3(rotation[0],rotation[1],rotation[2]);
+    }
+    public Quaternion toQuaternion(){
+        if(rotation==null || rotation.length!=4)return new Quaternion(0f,0f,0f,1f);
+        return new Quaternion(rotation[0],rotation[1],rotation[2],rotation[3]);
     }
 }
 
