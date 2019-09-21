f3fcdc3b68e2930eadc18b2fb3e31458a516438f
沈徐乐 <crease.xu@gmail.com>
2019-09-21 14:38:48 +0800

    添加文字转语音的功能

--- samples/solarsystem/app/src/main/java/com/google/ar/sceneform/samples/solarsystem/SolarActivity.java
+++ samples/solarsystem/app/src/main/java/com/google/ar/sceneform/samples/solarsystem/SolarActivity.java
@@ -16,19 +16,28 @@
 package com.google.ar.sceneform.samples.solarsystem;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.ardog.model.DogPoint;
 import com.ardog.models.ModelLoaderManager;
 import com.ardog.utils.DrawLineHelper;
@@ -55,11 +64,19 @@ import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechSynthesizer;
+import com.iflytek.cloud.SynthesizerListener;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 
 /**
@@ -76,6 +93,72 @@ public class SolarActivity extends AppCompatActivity {
   private boolean hasPlacedSolarSystem = false;
   private List<Anchor> anchors = new ArrayList<>();
   private PathFinder pathFinder;
+
+    // 语音合成对象
+    private SpeechSynthesizer mTts;
+    // 引擎类型
+    private String mEngineType = SpeechConstant.TYPE_CLOUD;
+    /**
+     * 初始化监听。
+     */
+    private InitListener mTtsInitListener = new InitListener() {
+        @Override
+        public void onInit(int code) {
+            if (code != ErrorCode.SUCCESS) {
+                ToastUtils.showShort("初始化失败,错误码：" + code);
+            } else {
+                // 初始化成功，之后可以调用startSpeaking方法
+                // 注：有的开发者在onCreate方法中创建完合成对象之后马上就调用startSpeaking进行合成，
+                // 正确的做法是将onCreate中的startSpeaking调用移至这里
+            }
+        }
+    };
+    /**
+     * 合成回调监听。
+     */
+    private SynthesizerListener mTtsListener = new SynthesizerListener() {
+
+        @Override
+        public void onSpeakBegin() {
+        }
+
+        @Override
+        public void onSpeakPaused() {
+        }
+
+        @Override
+        public void onSpeakResumed() {
+        }
+
+        @Override
+        public void onBufferProgress(int percent, int beginPos, int endPos, String info) {
+        }
+
+        @Override
+        public void onSpeakProgress(int percent, int beginPos, int endPos) {
+        }
+
+        @Override
+        public void onCompleted(SpeechError error) {
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            //		Log.d(TAG, "session id =" + sid);
+            //	}
+
+            //当设置SpeechConstant.TTS_DATA_NOTIFY为1时，抛出buf数据
+			/*if (SpeechEvent.EVENT_TTS_BUFFER == eventType) {
+						byte[] buf = obj.getByteArray(SpeechEvent.KEY_EVENT_TTS_BUFFER);
+						Log.e("MscSpeechLog", "buf is =" + buf);
+					}*/
+
+        }
+    };
 
   @Override
   @SuppressWarnings({ "AndroidApiChecker", "FutureReturnValueIgnored" })
@@ -157,6 +240,8 @@ public class SolarActivity extends AppCompatActivity {
         return false;
       }
     });
+      // 初始化合成对象
+      mTts = SpeechSynthesizer.createSynthesizer(this, mTtsInitListener);
   }
 
   private void loadModels() {
@@ -334,10 +419,71 @@ public class SolarActivity extends AppCompatActivity {
             //faceToCameraNode.setLocalRotation(Quaternion.axisAngle(new Vector3(0f, 1f, 0f), 0f));
             faceToCameraNode.setLocalPosition(new Vector3(0f, 0.1f, 0f));
             faceToCameraNode.setRenderable(viewRenderable);
+              View view = viewRenderable.getView();
+              TextView tvName = view.findViewById(R.id.tv_name);
+              view.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                      sayHello(tvName.getText().toString());
+                  }
+              });
           }
         });
   }
+    /**
+     * 文字转语音
+     */
+    private void sayHello(String texts) {
+        // 设置参数
+        setParam();
+        int code = mTts.startSpeaking(texts, mTtsListener);
+//			/**
+//			 * 只保存音频不进行播放接口,调用此接口请注释startSpeaking接口
+//			 * text:要合成的文本，uri:需要保存的音频全路径，listener:回调接口
+//			*/
+			/*String path = Environment.getExternalStorageDirectory()+"/tts.pcm";
+			int code = mTts.synthesizeToUri(text, path, mTtsListener);*/
 
+        if (code != ErrorCode.SUCCESS) {
+            ToastUtils.showShort("语音合成失败,错误码: " + code);
+        }
+    }
+
+    /**
+     * 参数设置
+     *
+     * @return
+     */
+    private void setParam() {
+        // 清空参数
+        mTts.setParameter(SpeechConstant.PARAMS, null);
+        // 根据合成引擎设置相应参数
+        if (mEngineType.equals(SpeechConstant.TYPE_CLOUD)) {
+            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+            //支持实时音频返回，仅在synthesizeToUri条件下支持
+            //mTts.setParameter(SpeechConstant.TTS_DATA_NOTIFY, "1");
+            // 设置在线合成发音人
+            mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaorong");
+            //设置合成语速
+            mTts.setParameter(SpeechConstant.SPEED, "50");
+            //设置合成音调
+            mTts.setParameter(SpeechConstant.PITCH, "50");
+            //设置合成音量
+            mTts.setParameter(SpeechConstant.VOLUME, "50");
+        } else {
+            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
+            mTts.setParameter(SpeechConstant.VOICE_NAME, "");
+
+        }
+        //设置播放器音频流类型
+        mTts.setParameter(SpeechConstant.STREAM_TYPE, "3");
+        // 设置播放合成音频打断音乐播放，默认为true
+        mTts.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "true");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        mTts.setParameter(SpeechConstant.AUDIO_FORMAT, "pcm");
+        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/tts.pcm");
+    }
   private void hideLoadingMessage() {
     if (loadingMessageSnackbar == null) {
       return;
